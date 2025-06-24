8f239d3e4c0bcce52db64cba1a6c2dfd045eb2c0
Small_Fox <small_fox@HPZHAN66>
2025-06-24 16:26:03 +0800

    [HPZHAN66] 起项目后的第一次提交

--- src/main/java/top/smallfox/MainApp.java
+++ src/main/java/top/smallfox/MainApp.java
@@ -5,6 +5,7 @@ public class MainApp {
     public static void main(String[] args) {
         System.out.println("[ArchFox] 起项目起项目");
         System.out.println("[ArchFox] 起项目后的第一次提交");
+        System.out.println("[HPZHAN66] 起项目后的第一次提交");
     }
 
 }
